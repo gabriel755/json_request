0f4d21fd494d4098b1fc9c4f88c1dee17f100ad4
gabriel775 <gabrielfiamete@gmail.com>
2021-02-18 00:40:34 -0300

    Reques Json

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+
+    /*$content = '
+    <rss>
+        <informacao>
+            <item>
+                <title>Gabriel</title>
+                <idade>24</idade>
+            </item>
+        </informacao>
+    </rss>
+    '
+    $dir = mkdir('pasta_xml')
+
+    if(isset($dir)){
+        opendir($dir);
+            file_put_contents('./pasta_xml/file.xml', $content);
+        closedir($dir);
+    }else{
+        mkdir('pasta_xml');
+            opendir('pasta_xml');
+                file_put_contents('./pasta_xml/file.xml');
+            closedir('pasta_xml');
+    }*/
+
+    //Request
+    $arr = ['nome' => 'Gabriel', 'cidade' => 'Floripa'];
+
+    $json = json_encode($arr);
+
+    echo $json;
+
+    
+
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    require("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script>
+        $(function(){
+            $.ajax({
+                url:'config.php',
+                dataType:'json'
+            }).done(function(data){
+                console.log(data.nome);
+            })
+        })
+    </script>
+</body>
+</html>
